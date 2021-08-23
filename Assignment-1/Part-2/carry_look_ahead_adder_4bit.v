2b50ea87ea221bfd9be36abf48390e6b3a5c455b
Suhas Jain <suhasjain@iitkgp.ac.in>
2021-08-23 16:40:43 +0530

    modified a small part

--- Assignment-1/Part-2/carry_look_ahead_adder_4bit.v
+++ Assignment-1/Part-2/carry_look_ahead_adder_4bit.v
@@ -12,15 +12,8 @@ module carry_look_ahead_adder_4bit (
     wire [3:0] p, g;
     wire [4:0] c;
 
-    assign p[0] = (i_term1[0] ^ i_term2[0]);
-    assign p[1] = (i_term1[1] ^ i_term2[1]);
-    assign p[2] = (i_term1[2] ^ i_term2[2]);
-    assign p[3] = (i_term1[3] ^ i_term2[3]);
-
-    assign g[0] = (i_term1[0] & i_term2[0]);
-    assign g[1] = (i_term1[1] & i_term2[1]);
-    assign g[2] = (i_term1[2] & i_term2[2]);
-    assign g[3] = (i_term1[3] & i_term2[3]);
+    assign p = i_term1 ^ i_term2;
+    assign g = i_term1 & i_term2;
 
     assign c[0] = i_carry;
     assign c[1] = g[0] | (p[0] & i_carry);
@@ -28,10 +21,7 @@ module carry_look_ahead_adder_4bit (
     assign c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & i_carry);
     assign c[4] = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & i_carry);
 
-    assign o_sum[0] = p[0] ^ c[0];
-    assign o_sum[1] = p[1] ^ c[1];
-    assign o_sum[2] = p[2] ^ c[2];
-    assign o_sum[3] = p[3] ^ c[3];
+    assign o_sum = p ^ c;
 
     assign o_carry = c[4];
 
