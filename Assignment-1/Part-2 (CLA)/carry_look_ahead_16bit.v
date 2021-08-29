4a0f83a531e730b82f4b42dc46c997c6e6730035
mentaltraffic <58178738+mentaltraffic@users.noreply.github.com>
2021-08-29 19:37:11 +0530

    Update carry_look_ahead_16bit.v

--- Assignment-1/Part-2 (CLA)/carry_look_ahead_16bit.v
+++ Assignment-1/Part-2 (CLA)/carry_look_ahead_16bit.v
@@ -8,10 +8,10 @@ Suhas Jain and Monal Prasad
 module carry_look_ahead_16bit(A, B, Ci, S, Co, PG1, GG1);
     
     /*
-        A: 4-bit input to add 
-        B: 4-bit input to add 
+        A: 16-bit input to add 
+        B: 16-bit input to add 
         Ci: Input carry bit 
-        S: 4-bit output sum 
+        S: 16-bit output sum 
         Co: Output carry bit 
         PG1: Output PG bits
         GG1: Output GG bits
