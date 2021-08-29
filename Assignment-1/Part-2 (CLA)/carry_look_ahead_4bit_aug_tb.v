56dfd72948d85fb65f5f06d75caff3a3beefd8a0
mentaltraffic <58178738+mentaltraffic@users.noreply.github.com>
2021-08-29 18:45:16 +0530

    added comments

--- Assignment-1/Part-2 (CLA)/carry_look_ahead_4bit_aug_tb.v
+++ Assignment-1/Part-2 (CLA)/carry_look_ahead_4bit_aug_tb.v
@@ -9,15 +9,27 @@ Suhas Jain and Monal Prasad
 
 module carry_look_ahead_4bit_aug_tb;
     
+    /*
+        A: 3-bit input to add 
+        B: 3-bit input to add 
+        Ci: Input carry bit 
+        S: 3-bit output sum 
+        Co: Output carry bit 
+        PG: Output Group Propagate bit
+        GG: Output Group Generate bit
+    */
     reg [3:0] A = 4'b0, B = 4'b0;
     reg Ci = 1'b0;
     wire [3:0] S;
     wire Co, PG, GG;
 
     carry_look_ahead_4bit_aug carry_look_ahead(A, B, Ci, S, Co, PG, GG);
+    //Connecting the registers and wires to actual 4-bit augmented CLA design using structural modeling
 
     initial begin
         $monitor("A = %b, B = %b, Ci = %b, S = %b, Co = %b, PG = %b, GG = %b", A, B, Ci, S, Co, PG, GG);
+        // Printing the output after each iteration 
+        // Assigning different values to input bits and testing the outputs
         #10
         A = 4'b0100;
         B = 4'b0101;
