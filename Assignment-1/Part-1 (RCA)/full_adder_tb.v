60c27e84a2bc02ff68001ddc12add581483032c5
Suhas Jain <suhasjain@iitkgp.ac.in>
2021-08-29 16:09:06 +0530

    Added comments

--- Assignment-1/Part-1 (RCA)/full_adder_tb.v
+++ Assignment-1/Part-1 (RCA)/full_adder_tb.v
@@ -8,17 +8,24 @@ Suhas Jain and Monal Prasad
 `timescale 1ns/1ps
 
 module full_adder_tb;
- 
-    reg A = 1'b0;
-    reg B = 1'b0;
+    /*
+        A: Input bit to add
+        B: Input bit to add
+        Ci: Input carry bit 
+        S: Output sum bit
+        Co: Output carry bit
+    */
+
+    reg A = 1'b0, B = 1'b0;
     reg Ci = 1'b0;
     wire S;
     wire Co;
     
-    full_adder full_adder_inst(A, B, Ci, S, Co);
+    full_adder full_adder_inst(A, B, Ci, S, Co);  //Connecting the registers and wires to actual full adder design using structural modeling
     
     initial begin
-        $monitor("A = %b, B = %b, Ci = %b, S = %b, Co = %b", A, B, Ci, S, Co);
+        $monitor("A = %b, B = %b, Ci = %b, S = %b, Co = %b", A, B, Ci, S, Co);  // Printing the output after each iteration
+        // Assigning different values to input bits and testing the outputs
         #10;
         A = 1'b0;
         B = 1'b0;
--- Assignment-1/Part-1 (RCA)/ripple_carry_adder_4bit.v
+++ Assignment-1/Part-1 (RCA)/ripple_carry_adder_4bit.v
@@ -7,13 +7,21 @@ Suhas Jain and Monal Prasad
 
 module  ripple_carry_adder_4bit (A, B, Ci, S, Co);
 
-    input [3:0] A, B;
+    /*
+        A: 3-bit input to add 
+        B: 3-bit input to add 
+        Ci: Input carry bit 
+        S: 3-bit output sum 
+        Co: Output carry bit 
+    */
+    input [3:0] A, B;  
     input Ci;
     output [3:0] S;
     output Co;
     wire [2:0] w_temp;
 
-    full_adder full_adder1(.A(A[0]), .B(B[0]), .Ci(Ci), .S(S[0]), .Co(w_temp[0]));
+    //Calculating sums by 4 individual full adders and rippling in the carry via wire w_temp    
+    full_adder full_adder1(.A(A[0]), .B(B[0]), .Ci(Ci), .S(S[0]), .Co(w_temp[0]));          
     full_adder full_adder2(.A(A[1]), .B(B[1]), .Ci(w_temp[0]), .S(S[1]), .Co(w_temp[1]));
     full_adder full_adder3(.A(A[2]), .B(B[2]), .Ci(w_temp[1]), .S(S[2]), .Co(w_temp[2]));
     full_adder full_adder4(.A(A[3]), .B(B[3]), .Ci(w_temp[2]), .S(S[3]), .Co(Co));
