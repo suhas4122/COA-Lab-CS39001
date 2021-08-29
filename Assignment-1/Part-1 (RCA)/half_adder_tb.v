eaeef657f14b50f223cabbbab753b4d6e1537427
mentaltraffic <58178738+mentaltraffic@users.noreply.github.com>
2021-08-29 19:03:36 +0530

    added comments

--- Assignment-1/Part-1 (RCA)/half_adder_tb.v
+++ Assignment-1/Part-1 (RCA)/half_adder_tb.v
@@ -8,16 +8,27 @@ Suhas Jain and Monal Prasad
 `timescale 1ns/1ps
 
 module half_adder_tb;
+
+   /*
+        A: Input bit to add
+        B: Input bit to add
+        Ci: Input carry bit 
+        S: Output sum bit
+        Co: Output carry bit
+    */
  
     reg A = 1'b0;
     reg B = 1'b0;
     wire S;
     wire Co;
     
-    half_adder half_adder_inst(A, B, S, Co);
+ half_adder half_adder_inst(A, B, S, Co); //Connecting the registers and wires to half adder design using structural modeling
+   
     
     initial begin
         $monitor("A = %b, B = %b, S = %b, Co = %b", A, B, S, Co);
+        // Printing the output after each iteration
+        // Assigning different values to input bits and testing the outputs
         #10
         A = 1'b0;
         B = 1'b0;
