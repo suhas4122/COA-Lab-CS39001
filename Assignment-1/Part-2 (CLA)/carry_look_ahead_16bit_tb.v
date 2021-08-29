200200d762e5cfd5b36cca57ba10c2d7e2f020a8
mentaltraffic <58178738+mentaltraffic@users.noreply.github.com>
2021-08-29 18:26:02 +0530

    added comments

--- Assignment-1/Part-2 (CLA)/carry_look_ahead_16bit_tb.v
+++ Assignment-1/Part-2 (CLA)/carry_look_ahead_16bit_tb.v
@@ -7,15 +7,27 @@ Suhas Jain and Monal Prasad
 
 module carry_look_ahead_16bit_tb;
 
+    /*
+        A: 3-bit input to add 
+        B: 3-bit input to add 
+        Ci: Input carry bit 
+        S: 3-bit output sum 
+        Co: Output carry bit 
+        PG1: Output PG bits
+        GG1: Output GG bits
+    */
     reg [15:0] A = 16'b0, B = 16'b0;
     reg Ci = 1'b0;
     wire [15:0] S;
     wire Co, PG, GG;
 
     carry_look_ahead_16bit carry_look_ahead(A, B, Ci, S, Co, PG, GG);
+    //Connecting the registers and wires to actual 16-bit CLA design using structural modeling
 
     initial begin
         $monitor("A = %b, B = %b, Ci = %b, S = %b, Co = %b PG = %b, GG = %b", A, B, Ci, S, Co, PG, GG);
+        // Printing the output after each iteration 
+        // Assigning different values to input bits and testing the outputs
         #10
         A = 16'b0100101001001010;
         B = 16'b0101101101011011;
