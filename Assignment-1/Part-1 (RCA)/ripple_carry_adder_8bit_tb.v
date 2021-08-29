4961252c1a7c1e6e8f2c8c7b6ee9726027f21ef6
mentaltraffic <58178738+mentaltraffic@users.noreply.github.com>
2021-08-29 19:04:54 +0530

    added comments

--- Assignment-1/Part-1 (RCA)/ripple_carry_adder_8bit_tb.v
+++ Assignment-1/Part-1 (RCA)/ripple_carry_adder_8bit_tb.v
@@ -8,16 +8,25 @@ Suhas Jain and Monal Prasad
 `timescale  1ns/1ps
 
 module ripple_carry_adder_8bit_tb;
-    
+     /*
+        A: 7-bit input to add 
+        B: 7-bit input to add 
+        Ci: Input carry bit 
+        S: 7-bit output sum 
+        Co: Output carry bit 
+    */
     reg [7:0] A = 8'b0, B = 8'b0;
     reg Ci = 1'b0;
     wire [7:0] S;
     wire Co;
 
     ripple_carry_adder_8bit ripple_carry_adder(A, B, Ci, S, Co);
+    //Connecting the registers and wires to actual 8-bit RCA design using structural modeling
 
     initial begin
         $monitor("A = %b, B = %b, Ci = %b, S = %b, Co = %b", A, B, Ci, S, Co);
+        // Printing the output after each iteration 
+        // Assigning different values to input bits and testing the outputs
         #10
         A = 8'b01001010;
         B = 8'b01011011;
