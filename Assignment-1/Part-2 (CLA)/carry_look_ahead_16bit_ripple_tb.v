3dfe81652bca1efb0c5d065b6e60e49ddf949e5d
mentaltraffic <58178738+mentaltraffic@users.noreply.github.com>
2021-08-29 18:21:03 +0530

    added comments

--- Assignment-1/Part-2 (CLA)/carry_look_ahead_16bit_ripple_tb.v
+++ Assignment-1/Part-2 (CLA)/carry_look_ahead_16bit_ripple_tb.v
@@ -9,15 +9,27 @@ Suhas Jain and Monal Prasad
 
 module carry_look_ahead_16bit_ripple_tb;
     
+     /*
+        A: 3-bit input to add 
+        B: 3-bit input to add 
+        Ci: Input carry bit 
+        S: 3-bit output sum 
+        Co: Output carry bit 
+    */
+    
     reg [15:0] A = 16'b0, B = 16'b0;
     reg Ci = 1'b0;
     wire [15:0] S;
     wire Co;
 
-    carry_look_ahead_16bit_ripple carry_look_ahead(A, B, Ci, S, Co);
+    carry_look_ahead_16bit_ripple carry_look_ahead(A, B, Ci, S, Co); 
+    //Connecting the registers and wires to actual 16-bit carry look ahead adder with ripple design using structural modeling
+
 
     initial begin
         $monitor("A = %b, B = %b, Ci = %b, S = %b, Co = %b", A, B, Ci, S, Co);
+        // Printing the output after each iteration 
+        // Assigning different values to input bits and testing the outputs
         #10
         A = 16'b0100101001001010;
         B = 16'b0101101101011011;
