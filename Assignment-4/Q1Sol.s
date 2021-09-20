fd6d790303e0d6319670bfbb87e1a5b00aa8e348
Suhas Jain <suhasjain@iitkgp.ac.in>
2021-09-20 08:06:26 +0530

    Started assignment 4

---
+++ Assignment-4/Q1Sol.s
@@ -0,0 +1,303 @@
+#    Assignment - 4
+#    Problem  - 1 (Transpose of a matrix)
+#    Semester Aut - 2021 
+#    Group - 8
+#    Name_1 : Suhas Jain 
+#    Roll_1 : 19CS30048
+#    Name_2 : Monal Prasad
+#    Roll_2 : 19CS30030
+
+.data 
+
+# Declaration of output strings 
+enter_message:
+    .asciiz "*** STORING A GP IN A MATRIX AND FINDING DETERMINANT *** \n\n"
+
+input_message: 
+    .asciiz "Enter four positive integers n, a and r (press enter after entering each):  \n"
+
+output_message1:
+    .asciiz "Matrix after storing the GP (matrix A): \n"
+
+output_message2:
+    .asciiz "\nDeterminant of matrix A: "
+
+white_space:
+    .asciiz " "
+
+new_line:
+    .asciiz "\n"
+
+end_message:
+    .asciiz "\n*********** THANK YOU *********** \n\n"
+
+.text
+.globl main
+
+    main:
+		jal		initStack
+		
+		# Printing welcome message when the program starts
+		la      $a0, enter_message
+		li      $v0, 4
+		syscall
+
+		# Printing message to make the 2-D array
+		la      $a0, input_message
+		li      $v0, 4
+		syscall
+
+        # Taking first number (n) as input in $s0 
+        li      $v0, 5
+        syscall
+        move    $s0, $v0
+
+        # Taking first number (a) as input in $s0 
+        li      $v0, 5
+        syscall
+        move    $s1, $v0
+
+        # Taking first number (r) as input in $s0 
+        li      $v0, 5
+        syscall
+        move    $s2, $v0    
+
+        mul     $a0, $s0, $s0
+        jal     mallocInStack
+        move    $s3, $v0
+        
+        mul     $t0, $s0, $s0
+        li      $t1, 0
+        move    $t2, $s1
+        move    $t3, $s3
+        j       loop1
+
+        loop1:
+            sw      $t2, 0($t3)
+            mul		$t2, $t2, $s2
+            addi    $t3, 4
+            addi    $t1, 1
+            blt     $t1, $t0, loop1
+        
+        la      $a0, new_line
+        li      $v0, 4
+        syscall
+
+        la      $a0, output_message1
+		li      $v0, 4
+		syscall
+        
+        move    $a0, $s0
+        move    $a1, $s3
+        jal     printMatrix
+	
+		move 	$a1, $s0
+		move 	$a0, $s3	
+		jal 	recursive_Det
+		move 	$t1, $v0
+
+
+		li 		$v0, 4
+		la 		$a0, output_message2
+		syscall
+		
+		
+		li 		$v0, 1
+		move 	$a0, $t1
+		syscall
+
+	exit_code:
+        # Printing thank you message 
+        la      $a0, end_message
+        li      $v0, 4
+        syscall
+
+        # System call to exit the code
+        li      $v0, 10
+        syscall 
+		
+	# Initialise the stack ($sp and $fp pointers)
+    initStack:
+        addi    $sp, $sp, -4        # Give 4 bytes to the stack to store the frame pointer
+        sw      $fp, 0($sp)         # Store the old frame pointer
+        move    $fp, $sp            # Set frame pointer as stack pointer 
+        jr      $ra                 # Return 
+
+    # Function to allocate space of size m*n in stack and return base pointer 
+    mallocInStack:
+        move    $t0, $a0            
+        sll     $t0, $t0, 2         # Calculating 4*m*n
+        add     $sp, $sp, $t0       # Allocating  4*m*n bytes of memory in stack 
+        move    $v0, $sp            # Store the value of base pointer to return 
+		jr      $ra                 # Return 
+
+    # Function to push an element to a stack 
+    pushToStack:
+        addi    $sp, $sp, -4        # Give 4 bytes to the stack to store the value
+        sw      $a0, 0($sp)         # Store the value 
+        jr      $ra                 # Return 
+
+    printMatrix:
+        move    $t0, $a0
+        move    $t1, $t0
+        move    $t2, $a1
+        li      $t3, 0
+        li      $t4, 0
+        mult    $t0, $t1
+        mflo    $t5
+    
+        print_loop:
+            lw      $a0, 0($t2)
+            li      $v0, 1
+            syscall
+
+            addi    $t2, 4
+            addi    $t3, 1
+            addi    $t4, 1
+
+            la      $a0, white_space
+            li      $v0, 4
+            syscall
+
+            beq     $t3, $t5, end_print_loop
+            beq     $t4, $t1, print_newline
+            j       print_loop
+
+            end_print_loop:
+                la      $a0, new_line
+                li      $v0, 4
+                syscall
+                jr      $ra 
+
+            print_newline:
+                la      $a0, new_line
+                li      $v0, 4
+                syscall
+                li      $t4, 0
+                j       print_loop 
+
+	recursive_Det:
+		li 		$s1, 0
+		li 		$t1, 1						# To compare with 1
+		bgt 	$a1, $t1, findDet			# Check to see if size of matrix not is 1x1
+		lw		$v0, 0($s2)					# Set return value if size if 1x1
+		jr 		$ra							# Return 
+
+		findDet: 
+			subu 	$sp, $sp, 32
+			sw 		$ra, 28($sp)
+			sw 		$fp, 24($sp)
+			addu 	$fp, $sp, 32
+			
+			move 	$t0, $a1
+			move 	$s0, $a0
+			sw 		$s0, 20($sp)
+			sw 		$t0, 16($sp)			
+			li 		$t1, 1
+			li 		$t2, 0	# Loop counter
+			
+		det_loop:	
+			beq 	$t2, $t0, end_det
+			sw 		$t1, 12($sp)
+			sw 		$t2, 8($sp)
+			sw 		$s1, 4($sp)
+			
+			addi 	$t1, $t0, -1
+			mul 	$a0, $t1, $t1
+			sll 	$a0, $a0, 2
+			li 		$v0, 9
+			syscall
+			move 	$s2, $v0
+			
+			move 	$a0, $t2
+			move 	$a1, $t0
+			move 	$a2, $s0
+			move 	$a3, $s2
+			jal 	getCoFactor
+			
+			
+			subu 	$t0, $t0, 1
+
+
+			move 	$a0, $s2
+			move 	$a1, $t0
+			jal 	recursive_Det
+			move 	$t4, $v0
+			
+			lw 		$t0, 16($sp)
+			lw 		$s0, 20($sp)
+			lw 		$t2, 8($sp)
+			lw 		$t1, 12($sp)
+			lw 		$s1, 4($sp)
+
+			li 		$t5, 0
+			addu 	$t5, $t5, $t2
+			sll 	$t5, $t5, 2
+			add 	$t5, $s0, $t5
+			lw 		$t3, 0($t5)
+			mul 	$t3, $t3, $t1
+			
+			mul 	$t3, $t3, $t4
+			addu 	$s1, $s1, $t3
+			neg 	$t1, $t1
+			addu 	$t2, $t2, 1
+
+			b 		det_loop
+
+	end_det: 
+		lw 		$ra, 28($sp)
+		lw 		$fp, 24($sp)
+		move 	$v0, $s1
+		addu 	$sp, $sp, 32
+		jr 		$ra
+	 
+	getCoFactor: 
+		move 	$t2, $a0		# a0=i	#q	
+		move 	$t0, $a1		# a1=n	#n	
+		move 	$s0, $a2		# a2 = address of original matrix	
+		move 	$s2, $a3		# a3 = address of temp matrix 
+			
+		li 		$t6, 0	#i	
+		li 		$t7, 0	#j	
+		li 		$t8, 1	#row
+		
+		subu 	$s6, $t0, 1
+
+		outer: 
+			beq 	$t8, $t0, break_outer
+			li 		$t9, 0	#col
+			li 		$t7, 0	#col
+
+			inner: 
+				beq 	$t9, $t0, break_inner
+				beq 	$t9, $t2, inc_col
+
+				mul 	$s3, $t6, $s6       # $t5 <-- width * i
+				add 	$s3, $s3, $t7       # $t5 <-- width * i + j
+				sll 	$s3, $s3, 2         # $t5 <-- 2^2 * (width * i + j)
+				add 	$s3, $s2, $s3       # $t5 <-- base address + (2^2 * (width * i + j))
+
+				mul 	$s4, $t8, $t0       # $t5 <-- width * i
+				add 	$s4, $s4, $t9       # $t5 <-- width * i + j	
+				sll 	$s4, $s4, 2         # $t5 <-- 2^2 * (width * i + j)
+				add 	$s4, $s0, $s4 
+
+				lw 		$s5, 0($s4)          # store input number into array
+				sw 		$s5, 0($s3)
+		
+				addu 	$t7, $t7, 1
+				addu 	$t9, $t9, 1
+				b 		inner
+
+
+			inc_col: 
+				addu 	$t9, $t9, 1
+				b 		inner
+
+			break_inner:
+				addu 	$t6, $t6, 1
+				addu 	$t8, $t8, 1	
+				b 		outer
+
+		break_outer: 
+			jr 		$ra
---
+++ Assignment-4/Q2Sol.s
@@ -0,0 +1,255 @@
+#    Assignment - 4
+#    Problem  - 2 (Recursive sort of 10 numbers)
+#    Semester Aut - 2021 
+#    Group - 8
+#    Name_1 : Suhas Jain 
+#    Roll_1 : 19CS30048
+#    Name_2 : Monal Prasad
+#    Roll_2 : 19CS30030
+
+.data 
+
+# Declaration of output strings 
+enter_message:
+    .asciiz "*** SORTING ARRAY OF 10 NUMBERS BY RECURSIVE SORT ***\n\n"
+
+input_message: 
+    .asciiz "Enter ten integers to be sorted (press enter after each integer): \n"
+
+output_message:
+    .asciiz "\nTen integers in ascending order are: "
+
+white_space:
+    .asciiz " "
+
+new_line:
+    .asciiz "\n"
+
+end_message:
+    .asciiz "\n\n*********** THANK YOU *********** \n\n"
+
+# Allocating space for the array
+.align 2
+array: .space 40
+
+.text
+.globl main 
+
+	main:
+        # Printing welcome message when the program starts
+		la      $a0, enter_message
+		li      $v0, 4
+		syscall
+
+		# Printing message before taking array as input
+		la      $a0, input_message
+		li      $v0, 4
+		syscall
+
+        # Initialising $t1 and loading adress of array in $t3
+		li      $t1, 1  				
+		la      $t3, array			
+
+        # Loop to take input from the user
+		input_Loop: 			
+	        li      $v0, 5 			        # Taking integer as input
+	        syscall
+	        move    $t0, $v0 		        # Moving input value to $t0
+	        addi    $t1, $t1, 1 	        # Increasing $t1(i) by 1
+	        sw      $t0, ($t3) 		        # Storing entered value at array[i]
+	        addi    $t3, $t3, 4 		    # Increasing address of $t3 to access next address
+	        bne     $t1, 11, input_Loop     # Get user input 10 times
+
+        # Calling function recursive sort to sort the array 
+        la      $a0, array                  # First argument = array address
+        addi    $a1, $zero, 0               # Second argument = start index
+        addi    $a2, $zero, 9               # Third argument = end index
+        jal     recursive_sort
+
+        la      $a0, array                  # First argument = array address
+        addi    $a1, $zero, 10              # Second argument = number of elements in the array
+        jal     printArray
+
+        j       exit_code
+		
+	# Initialise the stack ($sp and $fp pointers)
+    initStack:
+        addi    $sp, $sp, -4                # Give 4 bytes to the stack to store the frame pointer
+        sw      $fp, 0($sp)                 # Store the old frame pointer
+        move    $fp, $sp                    # Set frame pointer as stack pointer 
+        jr      $ra                         # Return 
+
+    # Function to push an element to a stack 
+    pushToStack:
+        addi    $sp, $sp, -4                # Give 4 bytes to the stack to store the value
+        sw      $a0, 0($sp)                 # Store the value 
+        jr      $ra                         # Return 
+
+    # Function to swap two elements of an array 
+    SWAP:
+        move    $t0, $a0                    # Loading argumen1 into temporary register
+        move    $t1, $a1                    # Loading argumen2 into temporary register
+        lw      $t2, 0($t0)                 # Extracting value from address 
+        lw      $t3, 0($t1)                 # Extracting value from address 
+        sw      $t2, 0($t1)                 # Saving back the swapped value
+        sw      $t3, 0($t0)                 # Saving back the swapped value 
+        jr $ra
+
+    # Printing the sorted array 
+    printArray:
+        # Loading address of array and initialising variables 				
+        move    $t0, $a0 		 
+        move    $t1, $a1	
+        la      $a0, output_message
+        li      $v0, 4
+        syscall
+
+        print_loop: 
+            # Check if loop has run 10 times 
+            ble     $t1, $zero, return 	        
+            lw      $a0, ($t0)              # Load word at current address 
+            li      $v0, 1 				        
+            syscall                         # Print word
+            
+            # Increment current word address by 4 and decrease $t1
+            addi    $t0, $t0, 4 		        
+            sub     $t1, $t1, 1    	        
+
+            # Print white space
+            la      $a0, white_space     		       
+            li      $v0, 4
+            syscall 
+
+            # Continue the loop 
+            j       print_loop 	
+
+        return:
+            jr      $ra				
+
+    recursive_sort:
+        j       push_to_stack
+
+    outer_while:
+        blt     $s1, $s2, inner_while1
+
+        j       restore_stack
+
+        inner_while1:            
+            lw      $t0, 4($sp)        
+            mul     $t2, $t0, 4       
+            addu    $t2, $t2, $s0
+            lw      $t3, 0($t2)
+            
+            lw      $t0, 0($sp)
+            
+            mul     $t2, $s1, 4        
+            addu    $t2, $t2, $s0       
+            lw      $t4, 0($t2)
+
+            blt     $t3, $t4, inner_while2   
+            ble     $t0, $s1, inner_while2
+
+            addu    $s1, 1 
+
+            j       inner_while1
+        
+        inner_while2:
+            lw      $t0, 4($sp) 
+
+            mul     $t2, $t0, 4        
+            addu    $t2, $t2, $s0      
+            lw      $t3, 0($t2)
+            
+            mul     $t2, $s2, 4         
+            addu    $t2, $t2, $s0     
+            lw      $t4, 0($t2)         
+
+            bge     $t0, $s2, check_if
+            bgt     $t3, $t4, check_if 
+
+            subu    $s2, 1            
+        
+            j       inner_while2
+
+        check_if:
+            bge     $s1, $s2, if_satisfied        
+            j       continue_while
+
+                if_satisfied:
+                    lw      $t0, 4($sp) 
+                    mul     $a0, $t0, 4          
+                    addu    $a0, $a0, $s0       
+                    mul     $a1, $s2, 4         
+                    addu    $a1, $a1, $s0      
+                    jal     SWAP                
+
+                    lw      $a0, 16($sp)           
+                    lw      $a1, 4($sp)        
+                    subu    $a2, $s2, 1        
+                    jal     recursive_sort      
+
+                    lw      $a0, 16($sp)            
+                    addu    $a1, $s2, 1        
+                    lw      $a2, 0($sp)       
+                    jal     recursive_sort
+
+                    j       restore_stack      
+
+        continue_while:
+            mul     $a0, $s1, 4          
+            addu    $a0, $a0, $s0       
+            mul     $a1, $s2, 4        
+            addu    $a1, $a1, $s0           
+            jal     SWAP 
+
+            j outer_while
+
+    initialise_variables:
+        la      $s0, array       
+        add     $s1, $a1, $zero      
+        add     $s2, $a2, $zero
+
+        j       outer_while
+
+    push_to_stack:
+        add     $a0, $ra, $zero      
+        jal     pushToStack
+
+        add     $a0, $s0, $zero      
+        jal     pushToStack
+
+        add     $a0, $s1, $zero      
+        jal     pushToStack
+
+        add     $a0, $s2, $zero      
+        jal     pushToStack
+
+        add     $a0, $a1, $zero      
+        jal     pushToStack
+
+        add     $a0, $a2, $zero      
+        jal     pushToStack
+
+        j       initialise_variables
+    
+    sort_return:
+        jr      $ra
+
+    restore_stack:
+        lw      $ra, 20($sp)
+        lw      $s0, 16($sp)
+        lw      $s1, 12($sp)
+        lw      $s2, 8($sp)
+        addu    $sp, $sp, 24
+
+        j       sort_return
+
+    exit_code:
+        # Printing thank you message 
+        la      $a0, end_message
+        li      $v0, 4
+        syscall
+
+        # System call to exit the code
+        li      $v0, 10
+        syscall 
