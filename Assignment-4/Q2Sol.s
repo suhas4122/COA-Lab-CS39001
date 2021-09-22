82bb15366f56bc4e55db50340ce6c994397b4817
mentaltraffic <58178738+mentaltraffic@users.noreply.github.com>
2021-09-22 17:03:37 +0530

    added comments- almost done

--- Assignment-4/Q2Sol.s
+++ Assignment-4/Q2Sol.s
@@ -36,185 +36,181 @@ array: .space 40
 .globl main 
 
 	main:
-        # Printing welcome message when the program starts
-		la      $a0, enter_message
+        	la      $a0, enter_message		# Printing welcome message
 		li      $v0, 4
 		syscall
 
-		# Printing message before taking array as input
+							# Prompting user to input array
 		la      $a0, input_message
 		li      $v0, 4
 		syscall
 
-        # Initialising $t1 and loading adress of array in $t3
-		li      $t1, 1  				
-		la      $t3, array			
+		li      $t1, 1  			# Initialising $t1				
+		la      $t3, array			# loading adress of array in $t3
 
         # Loop to take input from the user
-		input_Loop: 			
+	input_Loop: 			
 	        li      $v0, 5 			        # Taking integer as input
 	        syscall
+
 	        move    $t0, $v0 		        # Moving input value to $t0
-	        addi    $t1, $t1, 1 	        # Increasing $t1(i) by 1
+	        addi    $t1, $t1, 1 	        	# Increasing $t1(i) by 1
 	        sw      $t0, ($t3) 		        # Storing entered value at array[i]
-	        addi    $t3, $t3, 4 		    # Increasing address of $t3 to access next address
-	        bne     $t1, 11, input_Loop     # Get user input 10 times
+	        addi    $t3, $t3, 4 		    	# Increasing address of $t3 to access next address
+	        bne     $t1, 11, input_Loop     	# Get user input 10 times
 
         # Calling function recursive sort to sort the array 
-        la      $a0, array                  # First argument = array address
-        addi    $a1, $zero, 0               # Second argument = start index
-        addi    $a2, $zero, 9               # Third argument = end index
+        la      $a0, array                  		# First argument = array address
+        addi    $a1, $zero, 0               		# Second argument = start index
+        addi    $a2, $zero, 9               		# Third argument = end index
         jal     recursive_sort
 
-        la      $a0, array                  # First argument = array address
-        addi    $a1, $zero, 10              # Second argument = number of elements in the array
-        jal     printArray
+        la      $a0, array                  		# First argument = array address
+        addi    $a1, $zero, 10              		# Second argument = number of elements in the array
+        jal     printArray				# Function call to print sorted array
 
-        j       exit_code
+        j       exit_code				# jump control to exit_code block
 		
 	# Initialise the stack ($sp and $fp pointers)
     initStack:
-        addi    $sp, $sp, -4                # Give 4 bytes to the stack to store the frame pointer
-        sw      $fp, 0($sp)                 # Store the old frame pointer
-        move    $fp, $sp                    # Set frame pointer as stack pointer 
-        jr      $ra                         # Return 
+        addi    $sp, $sp, -4                		# Give 4 bytes to the stack to store the frame pointer
+        sw      $fp, 0($sp)                 		# Store the old frame pointer
+        move    $fp, $sp                    		# Set frame pointer as stack pointer 
+        jr      $ra                         		# Return 
 
     # Function to push an element to a stack 
     pushToStack:
-        addi    $sp, $sp, -4                # Give 4 bytes to the stack to store the value
-        sw      $a0, 0($sp)                 # Store the value 
-        jr      $ra                         # Return 
-
+        addi    $sp, $sp, -4                		# Give 4 bytes to the stack to store the value
+        sw      $a0, 0($sp)                 		# Store the value 
+        jr      $ra                        	 	# Return 
+	
     popFromStack:
-		lw      $v0, 0($sp)                 # Store the value 
-		addi    $sp, $sp, 4                 # Give 4 bytes to the stack to store the value
-        jr      $ra                         # Return 
+	lw      $v0, 0($sp)                 		# Store the value 
+	addi    $sp, $sp, 4                 		# Give 4 bytes to the stack to store the value
+        jr      $ra                         		# Return 
 
     # Function to swap two elements of an array 
     SWAP:
-        move    $t0, $a0                    # Loading argumen1 into temporary register
-        move    $t1, $a1                    # Loading argumen2 into temporary register
-        lw      $t2, 0($t0)                 # Extracting value from address 
-        lw      $t3, 0($t1)                 # Extracting value from address 
-        sw      $t2, 0($t1)                 # Saving back the swapped value
-        sw      $t3, 0($t0)                 # Saving back the swapped value 
+        move    $t0, $a0                    		# Loading argument1 into temporary register
+        move    $t1, $a1                    		# Loading argument2 into temporary register
+        lw      $t2, 0($t0)                 		# Extracting value from address 
+        lw      $t3, 0($t1)                 		# Extracting value from address 
+        sw      $t2, 0($t1)                 		# Saving back the swapped value
+        sw      $t3, 0($t0)                 		# Saving back the swapped value 
         jr $ra
 
     # Printing the sorted array 
     printArray:
-        # Loading address of array and initialising variables 				
-        move    $t0, $a0 		 
-        move    $t1, $a1	
-        la      $a0, output_message
+        			
+        move    $t0, $a0 				# Loads address of array in $t0 
+        move    $t1, $a1				# Stores length of array in $t1
+        la      $a0, output_message			# Prints output message
         li      $v0, 4
         syscall
 
         print_loop: 
-            # Check if loop has run 10 times 
-            ble     $t1, $zero, return 	        
-            lw      $a0, ($t0)              # Load word at current address 
+            ble     $t1, $zero, return 	        	# Check if loop has run 10 times 
+            lw      $a0, ($t0)              		# Load word at current address 
             li      $v0, 1 				        
-            syscall                         # Print word
-            
-            # Increment current word address by 4 and decrease $t1
-            addi    $t0, $t0, 4 		        
-            sub     $t1, $t1, 1    	        
+            syscall                         		# Print word
+             
+            addi    $t0, $t0, 4 			# Increment current word address by 4		        
+            sub     $t1, $t1, 1   			# decrease $t1 by 1      
 
-            # Print white space
-            la      $a0, white_space     		       
+            la      $a0, white_space     		# Print white space	       
             li      $v0, 4
             syscall 
 
-            # Continue the loop 
-            j       print_loop 	
+            j       print_loop 				# Continue the loop 
 
         return:
-            jr      $ra				
+            jr      $ra					# Return control to caller function	
+
 
     recursive_sort:
         j       push_to_stack
 
     outer_while:
-        blt     $s1, $s2, inner_while1
+        blt     $s1, $s2, inner_while1			# Checking if l < r 
 
-        j       restore_stack
+        j       restore_stack				# Jump to restore_stack when sorting is complete
 
         inner_while1:            
-            lw      $t0, 4($sp)        
+            lw      $t0, 4($sp)        			
             mul     $t2, $t0, 4       
-            addu    $t2, $t2, $s0
-            lw      $t3, 0($t2)
+            addu    $t2, $t2, $s0			# store address of A[p] in $t2
+            lw      $t3, 0($t2)				# store value of A[p] in $t3
             
-            lw      $t0, 0($sp)
+            lw      $t0, 0($sp)				# load value of right
             
-            mul     $t2, $s1, 4        
-            addu    $t2, $t2, $s0       
-            lw      $t4, 0($t2)
+            mul     $t2, $s1, 4        			
+            addu    $t2, $t2, $s0       		# store address of A[l]
+            lw      $t4, 0($t2)				# store value of A[l]
 
-            blt     $t3, $t4, inner_while2   
-            ble     $t0, $s1, inner_while2
+            blt     $t3, $t4, inner_while2   		# checking if A[p]<A[l]
+            ble     $t0, $s1, inner_while2		# checking if right<=l
 
-            addu    $s1, 1 
+            addu    $s1, 1 				# increment l if above conditions fail
 
-            j       inner_while1
+            j       inner_while1			# loop back for next iteration
         
         inner_while2:
-            lw      $t0, 4($sp) 
+            lw      $t0, 4($sp)				# load value of left
 
-            mul     $t2, $t0, 4        
-            addu    $t2, $t2, $s0      
-            lw      $t3, 0($t2)
+            mul     $t2, $t0, 4        			
+            addu    $t2, $t2, $s0      			# store address of A[p] in $t2
+            lw      $t3, 0($t2)				# store value of A[p] in $t3
             
             mul     $t2, $s2, 4         
-            addu    $t2, $t2, $s0     
-            lw      $t4, 0($t2)         
+            addu    $t2, $t2, $s0     			# store address of A[r]
+            lw      $t4, 0($t2)         		# store value of A[r]
 
-            bge     $t0, $s2, check_if
-            bgt     $t3, $t4, check_if 
+            bge     $t0, $s2, check_if			# checking if left >= r
+            bgt     $t3, $t4, check_if 			# checking if A[p] > A[r]
 
-            subu    $s2, 1            
+            subu    $s2, 1            			# decrement r if above conditions fail
         
-            j       inner_while2
+            j       inner_while2			# loop back for next iteration
 
         check_if:
-            bge     $s1, $s2, if_satisfied        
+            bge     $s1, $s2, if_satisfied      	# checking if l >= r  
             j       continue_while
 
                 if_satisfied:
-                    lw      $t0, 4($sp) 
+                    lw      $t0, 4($sp)
                     mul     $a0, $t0, 4          
-                    addu    $a0, $a0, $s0       
+                    addu    $a0, $a0, $s0       	# First argument = A[p]
                     mul     $a1, $s2, 4         
-                    addu    $a1, $a1, $s0      
-                    jal     SWAP                
+                    addu    $a1, $a1, $s0      		# Second argument = A[r]
+                    jal     SWAP                	# function call to SWAP
 
-                    lw      $a0, 16($sp)           
-                    lw      $a1, 4($sp)        
-                    subu    $a2, $s2, 1        
+                    lw      $a0, 16($sp)           	# First argument = address of A
+                    lw      $a1, 4($sp)        		# Second argument = value of left
+                    subu    $a2, $s2, 1        		# Third argument = r-1
                     jal     recursive_sort      
 
-                    lw      $a0, 16($sp)            
-                    addu    $a1, $s2, 1        
-                    lw      $a2, 0($sp)       
+                    lw      $a0, 16($sp)            	# First argument = address of A
+                    addu    $a1, $s2, 1        		# Second argument = r + 1
+                    lw      $a2, 0($sp)       		# Third argument = value of right
                     jal     recursive_sort
 
-                    j       restore_stack      
+                    j       restore_stack      		# jump to restore_stack after completion of loop
 
         continue_while:
             mul     $a0, $s1, 4          
-            addu    $a0, $a0, $s0       
-            mul     $a1, $s2, 4        
-            addu    $a1, $a1, $s0           
-            jal     SWAP 
+            addu    $a0, $a0, $s0       		# First argument=  value of A[l]
+            mul     $a1, $s2, 4        	
+            addu    $a1, $a1, $s0           		# Second argument =  value of A[r]
+            jal     SWAP 				# function call to SWAP
 
-            j outer_while
+            j 	    outer_while				# jump back to outer_while for next iteration
 
     initialise_variables:
-        la      $s0, array       
-        add     $s1, $a1, $zero      
-        add     $s2, $a2, $zero
+        la      $s0, array       			# Loads address of array in $s0
+        add     $s1, $a1, $zero      			# Stores value of 'left' 
+        add     $s2, $a2, $zero				# Stores value of 'right'
 
-        j       outer_while
+        j       outer_while				# jump back to outer_while
 
     push_to_stack:
         add     $a0, $ra, $zero      
