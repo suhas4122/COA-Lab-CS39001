e9fa573540806182aa211c94db559776662adb03
mentaltraffic <58178738+mentaltraffic@users.noreply.github.com>
2021-10-03 15:33:29 +0530

    Create full_adder.v

---
+++ Assignment-6/Q2/full_adder.v
@@ -0,0 +1,24 @@
+/*
+Computer Organisation and Architecture Lab
+Assignment no. 6
+Group no. 8
+Suhas Jain and Monal Prasad
+*/
+
+module full_adder (A, B, Ci, S, Co);
+    
+    /*
+        A: Input bit to add
+        B: Input bit to add
+        Ci: Input carry bit 
+        S: Output sum bit
+        Co: Output carry bit
+    */
+
+    input A, B, Ci;
+    output S, Co;
+
+    assign S = A ^ B ^ Ci;                         // Calulating sum bit by taking the XOR
+    assign Co = (A & B) | (B & Ci) | (Ci & A);     // Calculating output carry bit by taking OR of pairwise ANDs
+
+endmodule
