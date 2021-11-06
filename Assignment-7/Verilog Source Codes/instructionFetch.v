23ecf2cbf9d4da5119a2e3f3d30613056baf73ed
Suhas Jain <suhasjain@iitkgp.ac.in>
2021-11-07 01:10:46 +0530

    Started RISC

---
+++ Assignment-7/Verilog Source Codes/instructionFetch.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////
+// Assignment No: 7
+// Group No     : 8
+// Group Members: Suhas Jain    (19CS30048)
+//                Monal Prasad  (19CS30030)
+// Semester No  : 5 (Autumn 2021-22)
+//////////////////////////////////////////////////////////////////////////
+
+module instructionFetch(
+    input [31:0] PC,
+    input clk,
+    input reset, 
+    output wire [31:0] instuction
+);
+
+
+endmodule 
+
