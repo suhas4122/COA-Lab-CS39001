79766e4933626284a8a436b30fae005261d1e376
mentaltraffic <4pihoo@gmail.com>
2021-11-10 14:20:37 +0530

    mux_tb updated

--- Assignment-7/RISC_grp_8/Verilog_Source_Codes/MUX_tb.v
+++ Assignment-7/RISC_grp_8/Verilog_Source_Codes/MUX_tb.v
@@ -13,29 +13,30 @@
 module mux_32b_tb;
 
     // Declare inputs and outputs 
-    reg [31:0] in0;
-    reg [31:0] in1;
+    reg [31:0] a;
+    reg [31:0] b;
     reg s;
     wire [31:0] out;
 
     // Instantiate the Unit Under Test (UUT)
-    MUX_32b_2_to_1 UUT(.a(in0), .b(in1), .select(s), .out(out));
+    MUX_32b_2_to_1 UUT(.a(a), .b(b), .select(s), .out(out));
     
 
     initial begin
-        // Check all possible combinations of s, in0, in1
+        // Check all possible combinations of s, a, b
+
+            a = 51;
+            b = 122;
 
             #10
-            in0 = 51;
-            in1 = 22;
             s = 0;
-            $display("s:%d, in0:%d, in1:%d, out:%d", s, in0, in1, out);
+            #1
+            $display("s:%b, a:%b, b:%b, out:%b", s, a, b, out);
 
             #10
             s = 1;
             #1
-            $display("s:%d, in0:%d, in1:%d, out:%d", s, in0, in1, out);
-            
+            $display("s:%b, a:%b, b:%b, out:%b", s, a, b, out);
         
     end 
 
@@ -44,29 +45,32 @@ endmodule
 module mux_5b_tb;
 
     // Declare inputs and outputs 
-    reg [4:0] in0;
-    reg [4:0] in1;
+    reg [4:0] a;
+    reg [4:0] b;
     reg s;
     wire [4:0] out;
 
     // Instantiate the Unit Under Test (UUT)
-    MUX_5b_2_to_1 UUT(.a(in0), .b(in1), .select(s), .out(out));
+    MUX_5b_2_to_1 UUT(.a(a), .b(b), .select(s), .out(out));
     
 
 
     initial begin
-        // Check all possible combinations of s, in0, in1
+        // Check all possible combinations of s, a, b
+            
+            
+            a = 15;
+            b = 22;
 
             #10
-            in0 = 15;
-            in1 = 22;
             s = 0;
-            $display("s:%b, in0:%d, in1:%d, out:%d", s, in0, in1, out);
+            #1
+            $display("s:%b, a:%b, b:%b, out:%b", s, a, b, out);
 
             #10
             s = 1;
             #1
-            $display("s:%b, in0:%d, in1:%d, out:%d", s, in0, in1, out);
+            $display("s:%b, a:%b, b:%b, out:%b", s, a, b, out);
             
         
     end 
