dcc647a55b09366806f4c2bd5ad5ed6dd8ff110a
Suhas Jain <suhasjain@iitkgp.ac.in>
2021-11-12 22:26:13 +0530

    Almost done with this shit

--- Assignment-7/RISC_grp_8/Testing Files/Bubble Sort/bubble_sort.s
+++ Assignment-7/RISC_grp_8/Testing Files/Bubble Sort/bubble_sort.s
@@ -1,56 +1,63 @@
+# BUBBLE SORT OF 10 NUMBERS 
+# SUHAS JAIN | 19CS30048
+# MONAL PRASAD | 19CS30030
+
 main:
-    xor $20, $20  
-    xor $21, $21
-    addi $21, 10    
-    xor $8, $8     
-    xor $9, $9     
-    
+    xor $20, $20        # $20 = 0 (base address)
+    xor $21, $21        # $21 = 0 (n)
+    addi $21, 10        # $21 = n = 10
+    xor $8, $8          # $8 = 0 (outer loop variable (i))
+    xor $9, $9          # $9 = 0 (inner loop variable (j))
+
 fori:
-    xor $10, $10    
-    add $10, $8
-    comp $11, $21
-    add $10, $11
-    addi $10, 1    
-    bz $10, exitfori
-    xor $9, $9
+    xor $10, $10        # label 20
+    add $10, $8         # $10 = i
+    comp $11, $21       # $11 = -n 
+    add $10, $11        # $10 = i - n
+    addi $10, 1         # $10 = i - (n - 1) = i - n + 1
+    bz $10, exitfori    # if i = n-1, end outer loop 
+    xor $9, $9          # j = 0
 
 forj:
-    xor $11, $11    
-    add $11, $9
-    add $11, $10
-    bz $11, exitforj   
-    xor $12, $12
-    add $12, $9
-    shll $12, 2    
-    add $12, $20    
-    lw $13, 0($12)   
-    xor $4, $4
-    add $4, $12
-    addi $12, 4
-    lw $14, 0($12)   
-    xor $5, $5
-    add $5, $12
-    comp $15, $14
-    add $13, $15       
-    bltz $13, incj      
-    bz $13, incj
-    bl swap
+    xor $11, $11        # $11 = 0
+    add $11, $9         # $11 = j
+    add $11, $10        # $11 = j + i - n + 1
+    bz $11, exitforj    # if j = n - i - 1, end inner loop 
+
+    xor $12, $12        # $12 = 0
+    add $12, $9         # $12 = j 
+    shll $12, 2         # $12 = 4 * j
+    add $12, $20        # $12 =  arr + 4 * j
+    lw $13, 0($12)      # $13 = arr[j]
+
+    xor $4, $4          # $4 = 0
+    add $4, $12         # $4 = arr + 4 * j
+    addi $12, 4         # $4 = arr + 4 * j + 4
+    lw $14, 0($12)      # $14 = arr[j + 1]
+
+    xor $5, $5          # $5 = 0
+    add $5, $12         # $5 = arr + 4 * j
+    comp $15, $14       # $5 = -arr[j+1]
+    add $13, $15        # $13 = arr[j] - arr[j + 1]
+    bltz $13, incj      # don't swap if arr[j] > arr[j + 1]
+    bz $13, incj        # don't swap if arr[j] = arr[j + 1]
+    bl swap             # swap otherwise 
 
 incj:
-    addi $9, 1     
-    b forj
+    addi $9, 1          # j = j + 1
+    b forj              # continue inner loop 
 
 swap:
-    lw $18, 0($4)   
-    lw $19, 0($5)
-    sw $18, 0($5)
-    sw $19, 0($4)
+    lw $18, 0($4)       # $18 = arr[j]
+    lw $19, 0($5)       # $19 = arr[j + 1]
+    sw $18, 0($5)       # arr[j+1] = $18 (save in swpped location) 
+    sw $19, 0($4)       # arr[j] = $19 (save in swpped location) 
     br $31
 
 exitforj:
-    addi $8, 1      
-    b fori
+    addi $8, 1          # i = i + 1
+    b fori              # continue inner loop
 
 exitfori:
-    xor $16, $16       
-    addi $16, 1
+    xor $16, $16        # $16 = 0
+    addi $16, 1         # $16 = 1 (flag indication completion of execution)
--- Assignment-7/RISC_grp_8/Testing Files/Iterative Fibonacci/iterative_fibonacci.s
+++ Assignment-7/RISC_grp_8/Testing Files/Iterative Fibonacci/iterative_fibonacci.s
@@ -1,40 +1,36 @@
+# ITERATIVE FIBONACCI CALCULATION
+# SUHAS JAIN | 19CS30048
+# MONAL PRASAD | 19CS30030
+
 main:
-    xor $1, $1      # $1 = 0 (previous)
-    xor $2, $2      # $2 = 0 (current)
+    xor $1, $1      # $1 = 0 (previous element)
+    xor $2, $2      # $2 = 0 (current element)
     xor $3, $3      # $3 = 0 (loop variable (i) )
-    xor $4, $4      
-    xor $5, $5
-    addi $1, 1
-    addi $2, 1
-    addi $3, 2
-    addi $4, 6
+    xor $4, $4      # $4 = 0 (n)
+    xor $5, $5      # $5 = 0 (final answer)
+    addi $1, 1      # $1 = 1 (first element)
+    addi $2, 1      # $2 = 1 (second element)
+    addi $3, 2      # i = 2 (as we already have 2 elements)
+    addi $4, 6      # n = 6
 
 fib:
-    comp $6, $4     # - n
-    add $6, $3      # i - n
-    bz $6, endfib
+    comp $6, $4     # $6 = - n
+    add $6, $3      # $6 = i - n
+    bz $6, endfib   # $6 = 0  ==> i = n, end loop 
 
-    addi $6, -1
-    bz $6, endone
+    add $1, $2      # $1 = $1 + $2 
+    xor $7, $7      # $7 = 0
+    add $7, $1      # $7 = $1
+    xor $1, $1      # $1 = 0
+    add $1, $2      # $1 = $2
+    xor $2, $2      # $2 = 0
+    add $2, $7      # $2 = $7
 
-    add $1, $2
-    xor $7, $7
-    add $7, $1
-    xor $1, $1
-    add $1, $2
-    xor $2, $2
-    add $2, $7
+    addi $3, 1      # i = i + 1
+    b fib           # continue loop 
 
-    addi $3, 1
-    b fib
-
-endone:
-    add $5, $1
-    xor $8, $8
-    addi $8, 1
-
-endfib:
-    add $5, $2
-    xor $8, $8
-    addi $8, 1
+endfib:     
+    add $5, $2      # Final asnwer = $2
+    xor $8, $8      # $8 = 0 (completion flag)
+    addi $8, 1      # $8 = 1
 
